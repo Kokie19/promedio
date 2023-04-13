6d949161d260b9e98f003f40fb5c1e391867c642
Gaby_Kokie <gabytacruz447@gmail.com>
2023-04-13 12:40:29 -0600

    soy gaby

---
+++ src/Promedio.java
@@ -0,0 +1,22 @@
+public class Promedio {
+    public static void main (String[] args){
+
+        double  calificaciones = {8, 4, 7, 3, 6, 5};
+
+        double suma = CPromedio(calificaciones);
+        double promedio = CPromedio(calificaciones);
+
+        System.out.println("la suma es ");
+        System.out.println("Promedio ");
+    }
+    public static double Suma(double valores) {
+        }
+        return suma;
+
+
+    public static double CPromedio(double valores) {
+        double suma = CPromedio(valores);
+        return promedio;
+    }
+
+}
